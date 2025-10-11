1c9bdf8ca66fe5affa8a34ad73585b08e390f2af
ovjectibity <avigabhawala.aiesec@gmail.com>
2025-10-11 13:29:42 +0530

    db init fixes

--- db-init-scripts/01-create-databases.sql
+++ db-init-scripts/01-create-databases.sql
@@ -4,7 +4,10 @@ CREATE DATABASE assets;
 
 -- Create specific users
 CREATE USER enthalpy_app WITH PASSWORD 's5tarbus*niCr';
+CREATE USER enthalpy_admin WITH PASSWORD 'mor00nKiraj*niCr';
 
 -- Grant permissions
 GRANT ALL PRIVILEGES ON DATABASE common TO enthalpy_app;
 GRANT ALL PRIVILEGES ON DATABASE assets TO enthalpy_app;
+GRANT ALL PRIVILEGES ON DATABASE common TO enthalpy_admin;
+GRANT ALL PRIVILEGES ON DATABASE assets TO enthalpy_admin;
